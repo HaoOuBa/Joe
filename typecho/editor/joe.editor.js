b20e5dfaed436826af75fa0b9b96434d73d077a1
haoouba <2323333339@qq.com>
2021-02-03 20:11:16 +0800

    更新

--- typecho/editor/joe.editor.js
+++ typecho/editor/joe.editor.js
@@ -1,9 +1,18 @@
 $(function () {
 	$('#wmd-editarea textarea').attr('placeholder', '请输入文章内容...（支持图片粘贴上传）')
 	$('#wmd-button-bar .wmd-edittab').remove()
-    $('#wmd-button-row').html(`
-        <li>
+	$('#wmd-button-row .wmd-spacer').remove()
 
-        </li>
-    `)
+	/* 增加自定义功能 */
+	const items = [
+		{
+			title: '回复可见',
+			id: 'wmd-hide-button'
+		}
+	]
+
+	items.forEach(_ => {
+		let item = `<li class="wmd-button" id="${_.id}" title="${_.title}">啊</li>`
+		$('#wmd-button-row').append(item)
+	})
 })
