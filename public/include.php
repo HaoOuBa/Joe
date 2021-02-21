2e670269a690671e9b48e44ce3dffd984038898d
杜恒 <2323333339@qq.com>
2021-02-21 13:37:16 +0800

    更新

--- public/include.php
+++ public/include.php
@@ -1,31 +1,31 @@
 <script>
-  localStorage.getItem("data-night") && document.querySelector("html").setAttribute("data-night", "night");
-  window.Joe = {
-    LIVE2D: '<?php $this->options->JLive2d() ?>',
-    BASE_API: '<?php echo $this->options->rewrite == 0 ? '/index.php/joe/api' : '/joe/api' ?>',
-    DYNAMIC_BACKGROUND: '<?php $this->options->JDynamic_Background() ?>',
-    WALLPAPER_BACKGROUND_PC: '<?php $this->options->JWallpaper_Background_PC() ?>',
-    IS_MOBILE: /windows phone|iphone|android/gi.test(window.navigator.userAgent),
-    BAIDU_PUSH: <?php echo $this->options->JBaiduToken ? 'true' : 'false' ?>,
-    DOCUMENT_TITLE: '<?php $this->options->JDocumentTitle() ?>',
-    LAZY_LOAD: '<?php _getLazyload() ?>',
-    BIRTHDAY: '<?php $this->options->JBirthDay() ?>',
-  }
-  function detectIE(){var n=window.navigator.userAgent,e=n.indexOf("MSIE ");if(e>0){return parseInt(n.substring(e+5,n.indexOf(".",e)),10)}if(n.indexOf("Trident/")>0){var r=n.indexOf("rv:");return parseInt(n.substring(r+3,n.indexOf(".",r)),10)}var i=n.indexOf("Edge/");return i>0&&parseInt(n.substring(i+5,n.indexOf(".",i)),10)};
+	localStorage.getItem("data-night") && document.querySelector("html").setAttribute("data-night", "night");
+	window.Joe = {
+		LIVE2D: '<?php $this->options->JLive2d() ?>',
+		BASE_API: '<?php echo $this->options->rewrite == 0 ? '/index.php/joe/api' : '/joe/api' ?>',
+		DYNAMIC_BACKGROUND: '<?php $this->options->JDynamic_Background() ?>',
+		WALLPAPER_BACKGROUND_PC: '<?php $this->options->JWallpaper_Background_PC() ?>',
+		IS_MOBILE: /windows phone|iphone|android/gi.test(window.navigator.userAgent),
+		BAIDU_PUSH: <?php echo $this->options->JBaiduToken ? 'true' : 'false' ?>,
+		DOCUMENT_TITLE: '<?php $this->options->JDocumentTitle() ?>',
+		LAZY_LOAD: '<?php _getLazyload() ?>',
+		BIRTHDAY: '<?php $this->options->JBirthDay() ?>',
+	}
+	function detectIE(){var n=window.navigator.userAgent,e=n.indexOf("MSIE ");if(e>0){return parseInt(n.substring(e+5,n.indexOf(".",e)),10)}if(n.indexOf("Trident/")>0){var r=n.indexOf("rv:");return parseInt(n.substring(r+3,n.indexOf(".",r)),10)}var i=n.indexOf("Edge/");return i>0&&parseInt(n.substring(i+5,n.indexOf(".",i)),10)};
   detectIE() && (alert('当前站点不支持IE浏览器或您开启了兼容模式，请使用其他浏览器访问或关闭兼容模式。'), (location.href = 'https://www.baidu.com'))
 </script>
 <style>
-  body::before {
-    background: <?php if (_isMobile()) {
-                  echo $this->options->JWallpaper_Background_WAP ? "url(" . $this->options->JWallpaper_Background_WAP . ")" : "#f5f5f5";
-                } else {
-                  echo $this->options->JWallpaper_Background_PC ? "url(" . $this->options->JWallpaper_Background_PC . ")" : "#f5f5f5";
-                } ?>;
-    background-position: center 0;
-    background-repeat: no-repeat;
-    background-size: cover;
-  }
-  <?php $this->options->JCustomCSS() ?>
+	body::before {
+		background: <?php if (_isMobile()) {
+						echo $this->options->JWallpaper_Background_WAP ? "url(" . $this->options->JWallpaper_Background_WAP . ")" : "#f5f5f5";
+					} else {
+						echo $this->options->JWallpaper_Background_PC ? "url(" . $this->options->JWallpaper_Background_PC . ")" : "#f5f5f5";
+					} ?>;
+		background-position: center 0;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+	<?php $this->options->JCustomCSS() ?>
 </style>
 <meta charset="utf-8" />
 <meta name="renderer" content="webkit" />
@@ -38,11 +38,11 @@
 <link rel="shortcut icon" href="<?php $this->options->JFavicon() ?>" />
 <title><?php $this->archiveTitle(array('category' => '分类 %s 下的文章', 'search' => '包含关键字 %s 的文章', 'tag' => '标签 %s 下的文章', 'author' => '%s 发布的文章'), '', ' - '); ?><?php $this->options->title(); ?></title>
 <?php if ($this->is('single')) : ?>
-  <meta name="keywords" content="<?php echo $this->fields->keywords ? $this->fields->keywords : htmlspecialchars($this->_keywords); ?>" />
-  <meta name="description" content="<?php echo $this->fields->description ? $this->fields->description : htmlspecialchars($this->_description); ?>" />
-  <?php $this->header('keywords=&description='); ?>
+	<meta name="keywords" content="<?php echo $this->fields->keywords ? $this->fields->keywords : htmlspecialchars($this->_keywords); ?>" />
+	<meta name="description" content="<?php echo $this->fields->description ? $this->fields->description : htmlspecialchars($this->_description); ?>" />
+	<?php $this->header('keywords=&description='); ?>
 <?php else : ?>
-  <?php $this->header(); ?>
+	<?php $this->header(); ?>
 <?php endif; ?>
 <link rel="stylesheet" href="<?php $this->options->themeUrl('assets/css/joe.mode.css'); ?>">
 <link rel="stylesheet" href="<?php $this->options->themeUrl('assets/css/joe.normalize.css'); ?>">
@@ -50,7 +50,7 @@
 <link rel="stylesheet" href="<?php $this->options->themeUrl('assets/css/joe.responsive.css'); ?>">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/HaoOuBa/Joe@master/plugin/qmsg/qmsg.css" />
 <?php if ($this->options->JAside && in_array('weather', $this->options->JAside) && $this->options->JAside_Weather_Key) : ?>
-  <link rel="stylesheet" href="https://apip.weatherdt.com/standard/static/css/weather-standard.css">
+	<link rel="stylesheet" href="https://apip.weatherdt.com/standard/static/css/weather-standard.css">
 <?php endif; ?>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.7.2/animate.min.css" />
@@ -65,5 +65,5 @@
 <script src="<?php $this->options->themeUrl('assets/js/joe.global.js'); ?>"></script>
 <script async src="https://cdn.jsdelivr.net/gh/HaoOuBa/Joe@master/plugin/smooth/joe.smooth.js"></script>
 <?php if ($this->options->JCursorEffects && $this->options->JCursorEffects !== 'off') : ?>
-  <script async src="https://cdn.jsdelivr.net/gh/HaoOuBa/Joe@master/assets/cursor/<?php $this->options->JCursorEffects() ?>"></script>
+	<script async src="https://cdn.jsdelivr.net/gh/HaoOuBa/Joe@master/assets/cursor/<?php $this->options->JCursorEffects() ?>"></script>
 <?php endif; ?>
