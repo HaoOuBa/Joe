96f8074c003ca159e7876b10c523e150f7362dc7
杜恒 <2323333339@qq.com>
2021-04-14 14:45:37 +0800

    更新

--- public/include.php
+++ public/include.php
@@ -38,6 +38,6 @@
 <?php if ($this->options->JCursorEffects && $this->options->JCursorEffects !== 'off') : ?>
 	<script src="<?php $this->options->themeUrl('assets/cursor/' . $this->options->JCursorEffects); ?>" async></script>
 <?php endif; ?>
-<script src="<?php $this->options->themeUrl('assets/js/joe.global.min.js?v=20210414'); ?>"></script>
+<script src="<?php $this->options->themeUrl('assets/js/joe.global.min.js?v=202104141445'); ?>"></script>
 <script src="<?php $this->options->themeUrl('assets/js/joe.short.min.js?v=20210402'); ?>"></script>
 <?php $this->options->JCustomHeadEnd() ?>
