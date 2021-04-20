ad7290c6e7d7091f73c664a98880af97f682b623
杜恒 <2323333339@qq.com>
2021-04-20 10:04:43 +0800

    更新

--- core/factory.php
+++ core/factory.php
@@ -49,7 +49,7 @@ class Editor
 ?>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1.23.0/themes/prism-tomorrow.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="<?php Helper::options()->themeUrl('typecho/write/css/joe.write.min.css?v=202104191518') ?>">
+        <link rel="stylesheet" href="<?php Helper::options()->themeUrl('typecho/write/css/joe.write.min.css?v=202104200955') ?>">
         <script>
             window.JoeConfig = {
                 uploadAPI: '<?php Helper::security()->index('/action/upload'); ?>',
@@ -61,11 +61,10 @@ class Editor
                 themeURL: '<?php Helper::options()->themeUrl(); ?>'
             }
         </script>
-
         <script src="https://cdn.jsdelivr.net/npm/typecho-joe-next@6.2.4/plugin/prism/prism.min.js"></script>
-        <script src="<?php Helper::options()->themeUrl('typecho/write/js/joe.parse.min.js?v=202104191518') ?>"></script>
-        <script src="<?php Helper::options()->themeUrl('typecho/write/js/joe.write.chunk.js?v=202104191518') ?>"></script>
-        <script src="<?php Helper::options()->themeUrl('assets/js/joe.short.min.js?v=202104191518') ?>"></script>
+        <script src="<?php Helper::options()->themeUrl('typecho/write/js/joe.parse.min.js?v=202104200955') ?>"></script>
+        <script src="<?php Helper::options()->themeUrl('typecho/write/js/joe.write.chunk.js?v=202104200955') ?>"></script>
+        <script src="<?php Helper::options()->themeUrl('assets/js/joe.short.min.js?v=202104200955') ?>"></script>
 <?php
     }
 }
