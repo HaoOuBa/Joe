4eef566a6ae4c81f2640b53da4708d0cb38a75fb
haoouba <2323333339@qq.com>
2021-05-09 17:25:13 +0800

    更新

--- core/core.php
+++ core/core.php
@@ -29,6 +29,8 @@ function themeInit($self)
     Helper::options()->commentsRequireURL = false;
     /* 强制用户开启评论回复 */
     Helper::options()->commentsThreaded = true;
+    /* 强制回复楼层最高999层 */
+    Helper::options()->commentsMaxNestingLevels = 999;
     /* 主题开放API 路由规则 */
     $path_info = $self->request->getPathinfo();
 
--- core/function.php
+++ core/function.php
@@ -2,7 +2,7 @@
 /* 获取主题当前版本号 */
 function _getVersion()
 {
-	return "6.8.8";
+	return "6.8.9";
 };
 
 /* 判断是否是手机 */
