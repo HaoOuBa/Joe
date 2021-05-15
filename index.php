4546c4cdc890fb0ac42fe69498c01efe3badd911
Joe <admin@78.al>
2021-05-15 12:33:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * “ Eternity is not a distance but a decision - Joe ” <br /> “ 环境要求：PHP 5.4 ~ 7.4 ”
+ * “ 心中无女人，代码自然神 - 78.AL ” <br /> “ 环境要求：PHP 5.4 ~ 7.4 ”
  * @package Joe
  * @author Joe
  * @link https://78.al
